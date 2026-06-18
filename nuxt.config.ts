b55b454ab6f9ae3f18878b70cafe44575558f8d8
Matea Howard <bl0wfacee@gmail.com>
2026-06-18 09:03:57 +0100

    meta tags + favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,16 @@ export default defineNuxtConfig({
   css: ['~/assets/scss/main.scss'],
   app: {
     head: {
+      title: 'AI SEO Generator',
+      htmlAttrs: {
+        lang: 'en',
+      },
       link: [
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap',
         },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
     },
   },
